8ff6da67e4e00961f49ed31eb6c756229d7eb64b
Luis Raed <luis.raed@dataart.com>
2023-05-02 10:48:26 -0300

    Push frontend and backend code

---
+++ buy-and-sell-backend/app/routes/index.js
@@ -0,0 +1,8 @@
+import { getAllListingsRoute } from "./getAllListings.js";
+import { getAllListingRoute } from "./getListing.js";
+import { addViewToListingRoute } from "./addViewToListing.js";
+import { getUserListingsRoute } from "./getuserListings.js";
+import { createNewListingRoute } from "./createNewListing.js";
+import { updateListingRoute } from "./updateListing.js";
+import { deleteListingRoute } from "./deleteListing.js";
+export default [getAllListingsRoute, getAllListingRoute, addViewToListingRoute, getUserListingsRoute, createNewListingRoute, updateListingRoute, deleteListingRoute];
---
+++ buy-and-sell-backend/app/server.js
@@ -0,0 +1,35 @@
+import dotnev from 'dotenv';
+dotnev.config();
+import Hapi, { server } from '@hapi/hapi';
+import admin from 'firebase-admin';
+import routes from './routes/index.js';
+import { db } from './database.js';
+import credentials from '../credentials.json'; // assert { type: "json" };
+
+admin.initializeApp({
+  credential: admin.credential.cert(credentials)
+});
+let srv;
+const start = async () => {
+  srv = Hapi.server({
+    port: 8080,
+    host: '0.0.0.0'
+  });
+  routes.forEach(route => srv.route(route));
+  db.connect();
+  await srv.start();
+  console.log(`Server is listening on ${srv.info.uri}`);
+};
+process.on('unhandledRejection', err => {
+  console.log(err);
+  process.exit(1);
+});
+process.on('SIGINT', async () => {
+  console.log('Stopping server..');
+  await srv.stop({
+    setTimeout: 10000
+  });
+  db.end();
+  console.log('Server stopped');
+});
+start();
